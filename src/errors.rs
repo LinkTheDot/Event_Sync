559c9aecf7fb9f7e7d560bc3a63068b0c207ab12
LinkTheDot <CirnoStrongest9909@hotmail.com>
2023-04-23 09:57:13 -0400

    Renames the project to Event Sync and adds crate details to Cargo.toml
    In order to make the name of the project more clear, this commit renames
    any mention of the old name (Thread Sync), and changes it to Event Sync.
    
    In addition, this commit adds the required fields to the Cargo.toml file
    for uploading this project to crates.io.

--- src/errors.rs
+++ src/errors.rs
@@ -4,7 +4,7 @@ pub enum TimeError {
   /// This error is returned when the system clock has been rolled back to before the clock started.
   TimeHasReversed,
 
-  /// This error is returned when the [`wait_until()`](crate::ThreadSync::wait_until) method has been
+  /// This error is returned when the [`wait_until()`](crate::EventSync::wait_until) method has been
   /// called with a time that's already occurred.
   ThatTimeHasAlreadyHappened,
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,34 +1,39 @@
-//! ThreadSync is a crate that can be used to synchronize actions between threads.
+//! EventSync is a crate that can be used to synchronize events to only occurr between fixed gaps of time.
 //!
-//! ThreadSync can also be used to lock actions in one thread to only happen between fixed gaps of time.
+//! Say you wanted an event to occurr every 10ms, but it event takes a few milliseconds to setup that event.
+//! You'd end up having to sleep 10ms + the time it took to setup the event.
+//!
+//! That's where EventSync comes in.
+//! You can create an EventSync with a tickrate of 10ms, setup your event, then wait until the next tick.
+//! Aslong as the time it took to setup the event was <10ms, waiting for the next tick would ensure exactly 10ms had ocurred since the last event.
 //!
 //! # Getting Started
 //!
-//! In order to use thread_sync, you start by creating an instance of [`ThreadSync`](ThreadSync) with [`ThreadSync::new()`](ThreadSync::new).
-//! You then pass in the desired tickrate for the ThreadSync to know how long a tick should last.
+//! In order to use event_sync, you start by creating an instance of [`EventSync`](EventSync) with [`EventSync::new()`](EventSync::new).
+//! You then pass in the desired tickrate for the EventSync to know how long a tick should last.
 //!
 //! The tickrate will be an integer represented as milliseconds, and cannot go below 1.
 //! If you pass in 0, 1 millisecond will be set as the tickrate.
 //!
 //! ```
-//! use thread_sync::ThreadSync;
+//! use event_sync::EventSync;
 //!
 //! let tickrate = 10; // 10ms per tick
 //!
-//! // Create a thread synchronizer with a 10ms tickrate.
-//! let thread_sync = ThreadSync::new(tickrate);
+//! // Create an event synchronizer with a 10ms tickrate.
+//! let event_sync = EventSync::new(tickrate);
 //! ```
 //!
-//! With this, you can call methods such as [`wait_for_x_ticks()`](ThreadSync::wait_for_x_ticks).
+//! With this, you can call methods such as [`wait_for_x_ticks()`](EventSync::wait_for_x_ticks).
 //! Which will wait for the amount of ticks passed in.
 //!
 //! # What even is a ``Tick``?
 //!
-//! A ``Tick`` can be thought of as imaginary markers in time, starting at creation of the ThreadSync, and
+//! A ``Tick`` can be thought of as imaginary markers in time, starting at creation of the EventSync, and
 //! separated by the duration of the ``Tickrate``.
 //!
-//! When you wait for 1 tick, ThreadSync will sleep it's current thread up to the next tick.
-//! If you were to wait for multiple ticks, ThreadSync sleeps up to the next tick, plus the duration of the remaining ticks to wait for.
+//! When you wait for 1 tick, EventSync will sleep it's current thread up to the next tick.
+//! If you were to wait for multiple ticks, EventSync sleeps up to the next tick, plus the duration of the remaining ticks to wait for.
 
 use crate::errors::TimeError;
 use std::time::{Duration, SystemTime};
@@ -37,42 +42,42 @@ mod errors;
 
 /// A way to synchronize a dynamic number of threads through sleeping.
 ///
-/// This is achieved through cloning and passing around an instance of ThreadSync to other threads.
+/// This is achieved through cloning and passing around an instance of EventSync to other threads.
 ///
-/// ThreadSync can be used if you want actions between threads to happen at close to the same time.
-/// It can also be used if you want to regulate the time in which actions occurr.
+/// EventSync can be used if you want events between threads to happen at close to the same time.
+/// It can also be used if you want to regulate the time in which events occurr.
 ///
 /// # Usage
 ///
-/// In order to use threadsync you start by creating one with [`ThreadSync::new()`](ThreadSync::new).
-/// You then pass in the desired tickrate for the ThreadSync to use for how long a tick should last.
+/// In order to use eventsync you start by creating one with [`EventSync::new()`](EventSync::new).
+/// You then pass in the desired tickrate for the EventSync to use for how long a tick should last.
 ///
 /// The tickrate will be an integer in milliseconds, and cannot go below 1.
 /// If you pass in 0, 1 millisecond will be set as the tickrate.
 ///
 /// ```
-/// use thread_sync::ThreadSync;
+/// use event_sync::EventSync;
 ///
 /// let tickrate = 10; // 10ms per tick
 ///
-/// // Create a thread synchronizer with a 10ms tickrate.
-/// let thread_sync = ThreadSync::new(tickrate);
+/// // Create an event synchronizer with a 10ms tickrate.
+/// let event_sync = EventSync::new(tickrate);
 /// ```
 ///
-/// You can then use this ThreadSync for both time tracking and synchronizing threads.
+/// You can then use this EventSync for both time tracking and synchronizing threads.
 ///
 /// # Time Tracking
 /// ```
-/// use thread_sync::ThreadSync;
+/// use event_sync::EventSync;
 /// use std::time::Instant;
 ///
 /// let tickrate = 10; // 10ms per tick
-/// let thread_sync = ThreadSync::new(tickrate as u32);
+/// let event_sync = EventSync::new(tickrate as u32);
 ///
 /// let start = Instant::now();
 ///
 /// // Wait for 5 ticks (5 * 10)ms.
-/// thread_sync.wait_for_x_ticks(5);
+/// event_sync.wait_for_x_ticks(5);
 ///
 /// let finish = start.elapsed().as_millis();
 ///
@@ -82,39 +87,39 @@ mod errors;
 ///
 /// # Thread Synchronization
 /// ```
-/// use thread_sync::ThreadSync;
+/// use event_sync::EventSync;
 /// use std::thread;
 ///
 /// let tickrate = 10; // 10ms per tick
-/// let thread_sync = ThreadSync::new(tickrate);
+/// let event_sync = EventSync::new(tickrate);
 ///
-/// let passed_thread_sync = thread_sync.clone();
+/// let passed_event_sync = event_sync.clone();
 ///
 /// let handle = thread::spawn(move || {
-///   // waiting until 5 ticks have occurred since the creation of thread_sync.
-///   passed_thread_sync.wait_until(5);
+///   // waiting until 5 ticks have occurred since the creation of event_sync.
+///   passed_event_sync.wait_until(5);
 ///
 ///   // do something
 /// });
 ///
-/// // waiting until 5 ticks have occurred since the creation of thread_sync.
-/// thread_sync.wait_until(5);
+/// // waiting until 5 ticks have occurred since the creation of event_sync.
+/// event_sync.wait_until(5);
 ///
 /// // do something
 ///
 /// handle.join().unwrap();
 /// ```
 #[derive(Clone, Eq, PartialEq)]
-pub struct ThreadSync {
+pub struct EventSync {
   start_time: SystemTime,
   tickrate: u32,
 }
 
-impl ThreadSync {
-  /// Creates a new instance of ThreadSync.
+impl EventSync {
+  /// Creates a new instance of EventSync.
   ///
   /// Takes the duration of a tick as milliseconds.
-  /// If 0 is passed in, 1 will be the assigned tickrate for this instance of ThreadSync.
+  /// If 0 is passed in, 1 will be the assigned tickrate for this instance of EventSync.
   pub fn new(tickrate_in_milliseconds: u32) -> Self {
     let tickrate = if tickrate_in_milliseconds > 0 {
       tickrate_in_milliseconds
@@ -128,33 +133,33 @@ impl ThreadSync {
     }
   }
 
-  /// Waits until the passed in amount of ticks have occurred since ThreadSync creation.
-  // TODO fit the term absolute somewhere
+  // /// Waits until the passed in amount of ticks have occurred since EventSync creation.
+  /// Waits until an absolute tick has occurred since EventSync creation.
   ///
-  /// That means, if you created an instance of ThreadSync with a tickrate of 10ms,
+  /// That means, if you created an instance of EventSync with a tickrate of 10ms,
   /// and you want to wait until 1 second has passed since creation.
-  /// You would pass in 100 to this method, as 100 ticks would be 1 second since ThreadSync Creation.
+  /// You would wait until the 100th tick, as 100 ticks would be 1 second since EventSync Creation.
   ///
   /// # Usage
   ///
   /// ```
-  /// use thread_sync::ThreadSync;
+  /// use event_sync::EventSync;
   ///
   /// let tickrate = 10; // 10ms per tick
-  /// let thread_sync = ThreadSync::new(tickrate);
+  /// let event_sync = EventSync::new(tickrate);
   ///
-  /// // Wait 1 second from the creation of thread_sync.
-  /// thread_sync.wait_until(100).unwrap();
+  /// // Wait 1 second from the creation of event_sync.
+  /// event_sync.wait_until(100).unwrap();
   /// ```
   ///
   /// # Errors
   ///
-  /// - An error is returned when the system time has been reversed before this ThreadSync was created.
+  /// - An error is returned when the system time has been reversed before this EventSync was created.
   /// - An error is returned when the given time to wait for has already occurred.
   pub fn wait_until(&self, tick_to_wait_for: u64) -> Result<(), TimeError> {
     if self.ticks_since_started()? < tick_to_wait_for {
-      let time_to_wait_from_start = Duration::from_millis(tick_to_wait_for * self.tickrate as u64);
-      let total_time_to_wait = time_to_wait_from_start - self.time_since_started()?;
+      let total_time_to_wait = Duration::from_millis(tick_to_wait_for * self.tickrate as u64)
+        - self.time_since_started()?;
 
       std::thread::sleep(total_time_to_wait);
     } else {
@@ -164,41 +169,67 @@ impl ThreadSync {
     Ok(())
   }
 
-  /// TODO
-  // TODO fit the term relative somewhere
+  /// Waits until the next tick relative to where now is between ticks.
+  ///
+  /// Let's say the tickrate is 10ms, and the last tick was 5ms ago.
+  /// This method would sleep for 5ms to get to the next tick.
+  ///
+  /// # Usage
+  /// ```
+  /// use event_sync::EventSync;
+  ///
+  /// let tickrate = 10; // 10ms per tick
+  /// let event_sync = EventSync::new(tickrate);
+  ///
+  /// // wait until the next tick
+  /// event_sync.wait_for_tick();
+  /// ```
   pub fn wait_for_tick(&self) {
     self.wait_for_x_ticks(1);
   }
 
-  /// TODO
-  // TODO fit the term relative somewhere
+  /// Waits for the passed in amount of ticks relative to where now is between ticks.
+  ///
+  /// Let's say the tickrate is 10ms, and the last tick was 5ms ago.
+  /// If you wanted to wait for 3 ticks, this method would sleep for 25ms, as that would be 3 ticks from now.
+  ///
+  /// # Usage
+  /// ```
+  /// use event_sync::EventSync;
+  ///
+  /// let tickrate = 10; // 10ms per tick
+  /// let event_sync = EventSync::new(tickrate);
+  ///
+  /// // wait for 3 ticks
+  /// event_sync.wait_for_x_ticks(3);
+  /// ```
   pub fn wait_for_x_ticks(&self, ticks_to_wait: u32) {
     let ticks_since_started = self.ticks_since_started().unwrap();
 
     let _ = self.wait_until(ticks_since_started + ticks_to_wait as u64);
   }
 
-  /// Returns the amount of time that has occurred since the creation of this instance of ThreadSync.
+  /// Returns the amount of time that has occurred since the creation of this instance of EventSync.
   ///
   /// # Usage
   /// ```
-  /// use thread_sync::ThreadSync;
+  /// use event_sync::EventSync;
   /// use std::time::Duration;
   ///
   /// let tickrate = 10; // 10ms per tick
-  /// let thread_sync = ThreadSync::new(tickrate);
+  /// let event_sync = EventSync::new(tickrate);
   ///
-  /// // Wait until 5 ticks have occurred since ThreadSync creation.
-  /// thread_sync.wait_until(5);
+  /// // Wait until 5 ticks have occurred since EventSync creation.
+  /// event_sync.wait_until(5);
   ///
-  /// let milliseconds_since_started = thread_sync.time_since_started().unwrap().as_millis();
+  /// let milliseconds_since_started = event_sync.time_since_started().unwrap().as_millis();
   ///
   /// assert_eq!(milliseconds_since_started, 50);
   /// ```
   ///
   /// # Errors
   ///
-  /// - An error is returned when the system time has been reversed to before this ThreadSync was created.
+  /// - An error is returned when the system time has been reversed to before this EventSync was created.
   pub fn time_since_started(&self) -> Result<std::time::Duration, TimeError> {
     self
       .start_time
@@ -207,23 +238,23 @@ impl ThreadSync {
       .ok_or(TimeError::TimeHasReversed)
   }
 
-  /// Returns the amount of ticks that have occurred since the creation of this instance of ThreadSync.
+  /// Returns the amount of ticks that have occurred since the creation of this instance of EventSync.
   ///
   /// # Usage
   /// ```
-  /// use thread_sync::ThreadSync;
+  /// use event_sync::EventSync;
   ///
   /// let tickrate = 10; // 10ms per tick
-  /// let thread_sync = ThreadSync::new(tickrate);
+  /// let event_sync = EventSync::new(tickrate);
   ///
-  /// thread_sync.wait_until(5);
+  /// event_sync.wait_until(5);
   ///
-  /// assert_eq!(thread_sync.ticks_since_started(), Ok(5));
+  /// assert_eq!(event_sync.ticks_since_started(), Ok(5));
   /// ```
   ///
   /// # Errors
   ///
-  /// - An error is returned when the system time has been reversed to before this ThreadSync was created.
+  /// - An error is returned when the system time has been reversed to before this EventSync was created.
   pub fn ticks_since_started(&self) -> Result<u64, TimeError> {
     match self.start_time.elapsed() {
       Ok(time_since_started) => Ok(time_since_started.as_millis() as u64 / self.tickrate as u64),
@@ -245,24 +276,24 @@ mod tests {
 
     #[test]
     fn wait_until_valid_time() {
-      let thread_sync = ThreadSync::new(TEST_TICKRATE);
+      let event_sync = EventSync::new(TEST_TICKRATE);
 
-      thread_sync.wait_until(5).unwrap();
+      event_sync.wait_until(5).unwrap();
 
-      let ticks_since_started = thread_sync.ticks_since_started();
+      let ticks_since_started = event_sync.ticks_since_started();
 
       assert_eq!(ticks_since_started, Ok(5));
     }
 
     #[test]
     fn wait_until_passed_time() {
-      let thread_sync = ThreadSync::new(TEST_TICKRATE);
+      let event_sync = EventSync::new(TEST_TICKRATE);
 
       let expected_result = Err(TimeError::ThatTimeHasAlreadyHappened);
 
-      thread_sync.wait_for_x_ticks(3);
+      event_sync.wait_for_x_ticks(3);
 
-      let result = thread_sync.wait_until(1);
+      let result = event_sync.wait_until(1);
 
       assert_eq!(result, expected_result);
     }
@@ -270,33 +301,33 @@ mod tests {
 
   #[test]
   fn time_since_started_logic() {
-    let thread_sync = ThreadSync::new(TEST_TICKRATE);
+    let event_sync = EventSync::new(TEST_TICKRATE);
 
-    thread_sync.wait_until(2).unwrap();
+    event_sync.wait_until(2).unwrap();
 
-    let time_since_started = thread_sync.time_since_started().unwrap();
+    let time_since_started = event_sync.time_since_started().unwrap();
 
     assert_eq!(time_since_started.as_millis(), 20);
   }
 
   #[test]
   fn ticks_since_started_logic() {
-    let thread_sync = ThreadSync::new(TEST_TICKRATE);
+    let event_sync = EventSync::new(TEST_TICKRATE);
 
-    thread_sync.wait_until(2).unwrap();
+    event_sync.wait_until(2).unwrap();
 
-    let ticks_since_started = thread_sync.ticks_since_started();
+    let ticks_since_started = event_sync.ticks_since_started();
 
     assert_eq!(ticks_since_started, Ok(2));
   }
 
   #[test]
   fn wait_for_tick_logic() {
-    let thread_sync = ThreadSync::new(TEST_TICKRATE);
+    let event_sync = EventSync::new(TEST_TICKRATE);
 
-    thread_sync.wait_for_tick();
+    event_sync.wait_for_tick();
 
-    let ticks_since_started = thread_sync.ticks_since_started();
+    let ticks_since_started = event_sync.ticks_since_started();
 
     assert_eq!(ticks_since_started, Ok(1));
   }
