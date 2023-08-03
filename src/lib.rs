32a09cd240f80bb9e97a9c6067f05db75c1523be
LinkTheDot <CirnoStrongest9909@hotmail.com>
2023-08-03 15:27:48 -0400

    Change lib docs to pull from the README file.
    In order to cut down on documentation bloat in the lib file, this commit
    pulls the crate's documentation straight from the README file.

--- src/lib.rs
+++ src/lib.rs
@@ -1,39 +1,4 @@
-//! EventSync is a crate that can be used to synchronize events to only occurr between fixed gaps of time.
-//!
-//! Say you wanted an event to occurr every 10ms, but it event takes a few milliseconds to setup that event.
-//! You'd end up having to sleep 10ms + the time it took to setup the event.
-//!
-//! That's where EventSync comes in.
-//! You can create an EventSync with a tickrate of 10ms, setup your event, then wait until the next tick.
-//! Aslong as the time it took to setup the event was <10ms, waiting for the next tick would ensure exactly 10ms had ocurred since the last event.
-//!
-//! # Getting Started
-//!
-//! In order to use event_sync, you start by creating an instance of [`EventSync`](EventSync) with [`EventSync::new()`](EventSync::new).
-//! You then pass in the desired tickrate for the EventSync to know how long a tick should last.
-//!
-//! The tickrate will be an integer represented as milliseconds, and cannot go below 1.
-//! If you pass in 0, 1 millisecond will be set as the tickrate.
-//!
-//! ```
-//! use event_sync::EventSync;
-//!
-//! let tickrate = 10; // 10ms between every tick
-//!
-//! // Create an event synchronizer with a 10ms tickrate.
-//! let event_sync = EventSync::new(tickrate);
-//! ```
-//!
-//! With this, you can call methods such as [`wait_for_x_ticks()`](EventSync::wait_for_x_ticks).
-//! Which will wait for the amount of ticks passed in.
-//!
-//! # What even is a ``Tick``?
-//!
-//! A ``Tick`` can be thought of as imaginary markers in time, starting at creation of the EventSync, and
-//! separated by the duration of the ``Tickrate``.
-//!
-//! When you wait for 1 tick, EventSync will sleep it's current thread up to the next tick.
-//! If you were to wait for multiple ticks, EventSync sleeps up to the next tick, plus the duration of the remaining ticks to wait for.
+#![doc = include_str!("../README.md")]
 
 use crate::errors::TimeError;
 use std::time::{Duration, SystemTime};
@@ -282,7 +247,7 @@ impl EventSync {
   }
 }
 
-// Ticks have a chance to fail due to their time sensitive nature.
+// Tests have a chance to fail due to their time sensitive nature.
 #[cfg(test)]
 mod tests {
   use super::*;
