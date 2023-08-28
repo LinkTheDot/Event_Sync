4095f891d70c5d175b8589ad4a023cc942cc7be6
LinkTheDot <CirnoStrongest9909@hotmail.com>
2023-08-28 17:51:32 -0400

    Adds async waiting feature
    In order to make this project compatible with tokio, this commit adds a
    feature `async_waiting`. This feature swaps out all methods used for
    sleeping with asyncronous variants.

---
+++ src/async_waiting.rs
@@ -0,0 +1,38 @@
+use crate::errors::TimeError;
+use crate::EventSync;
+use std::time::Duration;
+
+#[async_trait::async_trait]
+pub trait AsyncWaiting {
+  async fn wait_until(&self, tick_to_wait_for: u64) -> Result<(), TimeError>;
+  async fn wait_for_tick(&self);
+  async fn wait_for_x_ticks(&self, ticks_to_wait: u32);
+}
+
+#[async_trait::async_trait]
+impl AsyncWaiting for EventSync {
+  async fn wait_until(&self, tick_to_wait_for: u64) -> Result<(), TimeError> {
+    if self.ticks_since_started()? < tick_to_wait_for {
+      let total_time_to_wait = Duration::from_millis(tick_to_wait_for * self.tickrate as u64)
+        - self.time_since_started()?;
+
+      tokio::time::sleep(total_time_to_wait).await;
+    } else {
+      return Err(TimeError::ThatTimeHasAlreadyHappened);
+    }
+
+    Ok(())
+  }
+
+  async fn wait_for_tick(&self) {
+    self.wait_for_x_ticks(1).await;
+  }
+
+  async fn wait_for_x_ticks(&self, ticks_to_wait: u32) {
+    let ticks_since_started = self.ticks_since_started().unwrap();
+
+    let _ = self
+      .wait_until(ticks_since_started + ticks_to_wait as u64)
+      .await;
+  }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,14 @@
 use crate::errors::TimeError;
 use std::time::{Duration, SystemTime};
 
+#[cfg(feature = "async_waiting")]
+pub use crate::async_waiting::AsyncWaiting;
+#[cfg(not(feature = "async_waiting"))]
+pub use crate::std_waiting::StdWaiting;
+
+mod async_waiting;
 mod errors;
+mod std_waiting;
 
 /// A way to synchronize a dynamic number of threads through sleeping.
 ///
@@ -21,7 +28,7 @@ mod errors;
 /// If you pass in 0, 1 millisecond will be set as the tickrate.
 ///
 /// ```
-/// use event_sync::EventSync;
+/// use event_sync::*;
 ///
 /// let tickrate = 10; // 10ms between every tick
 ///
@@ -33,7 +40,7 @@ mod errors;
 ///
 /// # Time Tracking
 /// ```
-/// use event_sync::EventSync;
+/// use event_sync::*;
 /// use std::time::Instant;
 ///
 /// let tickrate = 10; // 10ms between every tick
@@ -52,7 +59,7 @@ mod errors;
 ///
 /// # Thread Synchronization
 /// ```
-/// use event_sync::EventSync;
+/// use event_sync::*;
 /// use std::thread;
 ///
 /// let tickrate = 10; // 10ms between every tick
@@ -74,7 +81,7 @@ mod errors;
 ///
 /// handle.join().unwrap();
 /// ```
-#[derive(Clone, Eq, PartialEq)]
+#[derive(Debug, Clone, Eq, PartialEq)]
 pub struct EventSync {
   start_time: SystemTime,
   tickrate: u32,
@@ -98,86 +105,11 @@ impl EventSync {
     }
   }
 
-  /// Waits until an absolute tick has occurred since EventSync creation.
-  ///
-  /// That means, if you created an instance of EventSync with a tickrate of 10ms,
-  /// and you want to wait until 1 second has passed since creation.
-  /// You would wait until the 100th tick, as 100 ticks would be 1 second since EventSync Creation.
-  ///
-  /// # Usage
-  ///
-  /// ```
-  /// use event_sync::EventSync;
-  ///
-  /// let tickrate = 10; // 10ms between every tick
-  /// let event_sync = EventSync::new(tickrate);
-  ///
-  /// // Wait 1 second from the creation of event_sync.
-  /// event_sync.wait_until(100).unwrap();
-  /// ```
-  ///
-  /// # Errors
-  ///
-  /// - An error is returned when the system time has been reversed before this EventSync was created.
-  /// - An error is returned when the given time to wait for has already occurred.
-  pub fn wait_until(&self, tick_to_wait_for: u64) -> Result<(), TimeError> {
-    if self.ticks_since_started()? < tick_to_wait_for {
-      let total_time_to_wait = Duration::from_millis(tick_to_wait_for * self.tickrate as u64)
-        - self.time_since_started()?;
-
-      std::thread::sleep(total_time_to_wait);
-    } else {
-      return Err(TimeError::ThatTimeHasAlreadyHappened);
-    }
-
-    Ok(())
-  }
-
-  /// Waits until the next tick relative to where now is between ticks.
-  ///
-  /// Let's say the tickrate is 10ms, and the last tick was 5ms ago.
-  /// This method would sleep for 5ms to get to the next tick.
-  ///
-  /// # Usage
-  /// ```
-  /// use event_sync::EventSync;
-  ///
-  /// let tickrate = 10; // 10ms between every tick
-  /// let event_sync = EventSync::new(tickrate);
-  ///
-  /// // wait until the next tick
-  /// event_sync.wait_for_tick();
-  /// ```
-  pub fn wait_for_tick(&self) {
-    self.wait_for_x_ticks(1);
-  }
-
-  /// Waits for the passed in amount of ticks relative to where now is between ticks.
-  ///
-  /// Let's say the tickrate is 10ms, and the last tick was 5ms ago.
-  /// If you wanted to wait for 3 ticks, this method would sleep for 25ms, as that would be 3 ticks from now.
-  ///
-  /// # Usage
-  /// ```
-  /// use event_sync::EventSync;
-  ///
-  /// let tickrate = 10; // 10ms between every tick
-  /// let event_sync = EventSync::new(tickrate);
-  ///
-  /// // wait for 3 ticks
-  /// event_sync.wait_for_x_ticks(3);
-  /// ```
-  pub fn wait_for_x_ticks(&self, ticks_to_wait: u32) {
-    let ticks_since_started = self.ticks_since_started().unwrap();
-
-    let _ = self.wait_until(ticks_since_started + ticks_to_wait as u64);
-  }
-
   /// Returns the amount of time that has occurred since the creation of this instance of EventSync.
   ///
   /// # Usage
   /// ```
-  /// use event_sync::EventSync;
+  /// use event_sync::*;
   /// use std::time::Duration;
   ///
   /// let tickrate = 10; // 10ms between every tick
@@ -206,7 +138,7 @@ impl EventSync {
   ///
   /// # Usage
   /// ```
-  /// use event_sync::EventSync;
+  /// use event_sync::*;
   ///
   /// let tickrate = 10; // 10ms between every tick
   /// let event_sync = EventSync::new(tickrate);
@@ -244,107 +176,5 @@ impl EventSync {
   /// - An error is returned when the system time has been reversed to before this EventSync was created.
   pub fn time_until_next_tick(&self) -> Result<std::time::Duration, TimeError> {
     Ok(Duration::from_millis(self.tickrate as u64).saturating_sub(self.time_since_last_tick()?))
-  }
-}
-
-// Tests have a chance to fail due to their time sensitive nature.
-#[cfg(test)]
-mod tests {
-  use super::*;
-
-  /// Tickrate in milliseconds.
-  const TEST_TICKRATE: u32 = 10;
-
-  #[cfg(test)]
-  mod wait_until_logic {
-    use super::*;
-
-    #[test]
-    fn wait_until_valid_time() {
-      let event_sync = EventSync::new(TEST_TICKRATE);
-
-      event_sync.wait_until(5).unwrap();
-
-      let ticks_since_started = event_sync.ticks_since_started();
-
-      assert_eq!(ticks_since_started, Ok(5));
-    }
-
-    #[test]
-    fn wait_until_passed_time() {
-      let event_sync = EventSync::new(TEST_TICKRATE);
-
-      let expected_result = Err(TimeError::ThatTimeHasAlreadyHappened);
-
-      event_sync.wait_for_x_ticks(3);
-
-      let result = event_sync.wait_until(1);
-
-      assert_eq!(result, expected_result);
-    }
-  }
-
-  #[test]
-  fn time_since_started_logic() {
-    let event_sync = EventSync::new(TEST_TICKRATE);
-
-    event_sync.wait_until(2).unwrap();
-
-    let time_since_started = event_sync.time_since_started().unwrap();
-
-    assert_eq!(time_since_started.as_millis(), TEST_TICKRATE as u128 * 2);
-  }
-
-  #[test]
-  fn ticks_since_started_logic() {
-    let event_sync = EventSync::new(TEST_TICKRATE);
-
-    event_sync.wait_until(2).unwrap();
-
-    let ticks_since_started = event_sync.ticks_since_started();
-
-    assert_eq!(ticks_since_started, Ok(2));
-  }
-
-  #[test]
-  fn wait_for_tick_logic() {
-    let event_sync = EventSync::new(TEST_TICKRATE);
-
-    event_sync.wait_for_tick();
-
-    let ticks_since_started = event_sync.ticks_since_started();
-
-    assert_eq!(ticks_since_started, Ok(1));
-  }
-
-  #[test]
-  fn time_since_last_tick_logic() {
-    let event_sync = EventSync::new(TEST_TICKRATE);
-    let extra_wait_time = 2;
-
-    event_sync.wait_for_tick();
-
-    std::thread::sleep(Duration::from_millis(extra_wait_time as u64));
-
-    let time_since_last_tick = event_sync.time_since_last_tick().unwrap();
-
-    assert_eq!(time_since_last_tick.as_millis(), extra_wait_time);
-  }
-
-  #[test]
-  fn time_until_next_tick_logic() {
-    let event_sync = EventSync::new(TEST_TICKRATE);
-    let extra_wait_time = 2;
-
-    event_sync.wait_for_tick();
-
-    std::thread::sleep(Duration::from_millis(extra_wait_time as u64));
-
-    let time_since_last_tick = event_sync.time_until_next_tick().unwrap();
-
-    assert_eq!(
-      time_since_last_tick.as_millis(),
-      (TEST_TICKRATE as u128 - extra_wait_time)
-    );
   }
 }
