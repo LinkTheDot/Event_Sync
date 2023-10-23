4ccab78a055c375123fd350cc2a52f2bdbc753ca
LinkTheDot <CirnoStrongest9909@hotmail.com>
2023-10-23 18:07:37 -0400

    Implements pausing for EventSync
    In order to imlpement Serialize, this commit implements pausing for
    EventSync. This will allow the time that passed before storage to be
    retained.

--- examples/creation.rs
+++ examples/creation.rs
@@ -3,17 +3,21 @@
 use event_sync::*;
 use std::time::Duration;
 
-/// 10ms between ticks.
+/// 500ms between ticks.
 const TICKRATE: u32 = 500;
 
 fn main() {
   let starting_ticks = 2;
   let starting_time = Duration::from_millis((starting_ticks * TICKRATE).into());
 
+  // Create an EventSync from 0 with a tickrate of 500ms.
   let event_sync_zero = EventSync::new(TICKRATE);
+  // Create an EventSync with 2 ticks having already passed.
   let event_sync_from_ticks = EventSync::from_starting_tick(TICKRATE, starting_ticks);
+  // Create an EventSync with 1 second having already passed.
   let event_sync_from_time = EventSync::from_starting_time(TICKRATE, starting_time);
 
+  // Ensure all the starting times are as expected.
   assert_eq!(event_sync_zero.ticks_since_started(), Ok(0));
   assert_eq!(event_sync_from_ticks.ticks_since_started(), Ok(2));
   assert_eq!(event_sync_from_time.ticks_since_started(), Ok(2));
--- src/errors.rs
+++ src/errors.rs
@@ -2,8 +2,9 @@ use std::time::SystemTimeError;
 use thiserror::Error;
 
 /// All errors that can be returned when using this crate.
-#[derive(Error, Debug)]
+#[derive(Error, Debug, Clone)]
 pub enum TimeError {
+  #[allow(clippy::enum_variant_names)] // It's a wrapper, so I don't think this warning is valid here.
   #[error("An error has occurred when dealing with system time: {}", .0)]
   SystemTimeError(#[from] SystemTimeError),
 
@@ -11,6 +12,10 @@ pub enum TimeError {
   /// called with a time that's already occurred.
   #[error("A method with a time input has been told to wait for a time that already happened.")]
   ThatTimeHasAlreadyHappened,
+
+  /// Attempted to call a method on an EventSync that was paused.
+  #[error("Attempted to call a time based method on a paused EventSync.")]
+  EventSyncPaused,
 }
 
 impl PartialEq for TimeError {
